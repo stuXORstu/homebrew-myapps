0124d7e64bd625d4cda6a860b366d4bd676506e4
stuXORstu <stuXORstu@gmail.com>
2025-12-16 00:51:00 +1100

    Update to caffeine.rb

--- Casks/caffeine.rb
+++ Casks/caffeine.rb
@@ -1,17 +1,19 @@
 cask "caffeine" do
   version "1.6.2"
-  sha256 :no_check
+  sha256 "043da58fbee1ae8cfadcecbae6d337c3373ea16dca6e2a0bb469274e66967218"
 
-  if MacOS.version >= :tahoe
-    url "https://www.caffeine-app.net/download/tahoe"
-  else
-    url "https://www.caffeine-app.net/download"
-  end
+  url "https://github.com/stuXORstu/homebrew-myapps/releases/download/v#{version}/Caffeine-#{version}.zip",
+      verified: "github.com/stuXORstu/homebrew-myapps/"
 
   name "Caffeine"
-  desc "Prevent macOS from going to sleep"
-  homepage "https://www.caffeine-app.net/"
+  desc "Prevent system from going to sleep"
+  homepage "https://github.com/stuXORstu/homebrew-myapps"
 
   auto_updates true
   app "Caffeine.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.lightheadsw.caffeine.plist",
+    "~/Library/Application Support/Caffeine",
+  ]
 end
